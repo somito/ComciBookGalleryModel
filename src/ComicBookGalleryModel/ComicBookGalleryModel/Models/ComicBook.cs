d86aab9c61acf0374a4006391837f18e214b2423
hu3soan0 <hu3soan0@HU03ON20138>
2017-05-25 13:13:48 +0200

    created the database and the entity and context classes.

---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel.Models
+{
+    public class ComicBook
+    {
+        // ID, ComicBoocId, ComicBookID -> all usable for Id property
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedOn { get; set; }
+    }
+}
---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
@@ -0,0 +1,35 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                context.ComicBooks.Add(new ComicBook()
+                {
+                    SeriesTitle = "The Amazing Spider-Man",
+                    IssueNumber = 1,
+                    PublishedOn = DateTime.Today
+                });
+                context.SaveChanges();
+
+                var comicBooks = context.ComicBooks.ToList();
+                foreach (var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.SeriesTitle);
+                }
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
