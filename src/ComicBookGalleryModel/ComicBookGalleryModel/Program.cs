637624325b24f5b4bbb11286eb84f5ea63ee83b0
Soma <Soma@SOMA-PC>
2017-05-28 19:57:00 +0200

    added a LINQ query and a loging method.

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.Diagnostics;
 
 namespace ComicBookGalleryModel
 {
@@ -14,21 +15,30 @@ namespace ComicBookGalleryModel
         {
             using (var context = new Context())
             {
-                var comicBooks = context.ComicBooks
-                    .Include(cb => cb.Series)
-                    .Include(cb => cb.Artists.Select(a => a.Artist))
-                    .Include(cb => cb.Artists.Select(a => a.Role))
-                    .ToList();
-                foreach (var comicBook in comicBooks)
-                {
-                    var artistRoleNames = comicBook.Artists
-                        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
+                context.Database.Log = (message) => Debug.WriteLine(message);
 
-                    var artistRolesDisplayText = string.Join(",", artistRoleNames);
+                //var comicBooks = context.ComicBooks.ToList();
 
-                    Console.WriteLine(comicBook.DisplayText);
-                    Console.WriteLine(artistRolesDisplayText);
-                }
+                var comicBooksQuery = from cb in context.ComicBooks select cb;
+                var comicBooks = comicBooksQuery.ToList();
+
+                Console.WriteLine("# of comic books: {0}", comicBooks.Count);
+
+                //var comicBooks = context.ComicBooks
+                //    .Include(cb => cb.Series)
+                //    .Include(cb => cb.Artists.Select(a => a.Artist))
+                //    .Include(cb => cb.Artists.Select(a => a.Role))
+                //    .ToList();
+                //foreach (var comicBook in comicBooks)
+                //{
+                //    var artistRoleNames = comicBook.Artists
+                //        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
+
+                //    var artistRolesDisplayText = string.Join(",", artistRoleNames);
+
+                //    Console.WriteLine(comicBook.DisplayText);
+                //    Console.WriteLine(artistRolesDisplayText);
+                //}
 
                 Console.ReadLine();
 
